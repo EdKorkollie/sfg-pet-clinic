d3893d0c3acb6d0483579864a01b203730e4c4ef
EdKorkollie <50729522+EdKorkollie@users.noreply.github.com>
2020-05-23 12:55:47 -0400

    added PetType Service. Closes #29

---
+++ pet-clinic-data/src/main/java/edward/springframework/sfgpetclinic/service/PetTypeService.java
@@ -0,0 +1,6 @@
+package edward.springframework.sfgpetclinic.service;
+
+import edward.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
