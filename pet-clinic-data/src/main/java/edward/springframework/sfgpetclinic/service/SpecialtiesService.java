7ef2a0370e08c2e161cd2fc28b0622063e30483d
EdKorkollie <50729522+EdKorkollie@users.noreply.github.com>
2020-05-23 13:50:07 -0400

    added Specialties map impl. Closes #31

---
+++ pet-clinic-data/src/main/java/edward/springframework/sfgpetclinic/service/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package edward.springframework.sfgpetclinic.service;
+
+import edward.springframework.sfgpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
