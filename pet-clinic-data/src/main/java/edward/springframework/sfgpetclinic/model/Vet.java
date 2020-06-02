d75e930256012eafdb9f2a4aa656ea276a96c471
EdKorkollie <50729522+EdKorkollie@users.noreply.github.com>
2020-06-02 00:32:53 -0400

    converted Vet to JPA entities. Closes #37

--- pet-clinic-data/src/main/java/edward/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/edward/springframework/sfgpetclinic/model/Vet.java
@@ -1,10 +1,16 @@
 package edward.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
 
+    @ManyToMany(fetch = FetchType.EAGER )
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
+     inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Specialty> specialties = new HashSet<>();
 
     public Set<Specialty> getSpecialties() {
@@ -15,3 +21,11 @@ public class Vet extends Person{
         this.specialties = specialties;
     }
 }
+
+/**
+ * @ManyToMany(fetch = FetchType.EAGER ) // means by default anything that's a many relationship is lazily initialized. when set to eager
+ * that means JPA is going to try to load everything all at once.
+ * By Lazy initilization, which is default, it doesnt load until it is asked for so you will get back of that entity the specialties would be null
+ * if we did not do this.
+ *
+ */
