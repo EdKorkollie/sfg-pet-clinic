663f802a1c575fa8020961a586cbaab3701acf9a
EdKorkollie <50729522+EdKorkollie@users.noreply.github.com>
2020-06-02 00:10:44 -0400

    converted Owners to JPA entities. Closes #36

--- pet-clinic-data/src/main/java/edward/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/edward/springframework/sfgpetclinic/model/Owner.java
@@ -1,13 +1,23 @@
 package edward.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner") //cascade means if i have pet i delete the owner of the pet, the pet will also be deleted
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
